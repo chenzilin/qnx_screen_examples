ec766e523b3375534410efc42329bacb7dee7064
ZiLin Chen <chenzilin115@gmail.com>
2020-01-07 12:03:59 +0800

    add one more buffer to fix alpha error

--- example-png/main.c
+++ example-png/main.c
@@ -29,9 +29,21 @@ int main(void)
 
 	// set screen format to RGBA8888, 0xAARRGGBB
 	int screenFormat = SCREEN_FORMAT_RGBA8888;
-	screen_set_window_property_iv(screen_window, SCREEN_PROPERTY_FORMAT, &screenFormat);
+	err = screen_set_window_property_iv(screen_window, SCREEN_PROPERTY_FORMAT, &screenFormat);
 	if(err != 0){
 			printf("Failed to set format property\n");
+	}
+
+	int transparencyMode = SCREEN_TRANSPARENCY_SOURCE_OVER;
+	err = screen_set_window_property_iv(screen_window, SCREEN_PROPERTY_TRANSPARENCY, &transparencyMode);
+	if(err != 0){
+			printf("Failed to set transparency property\n");
+	}
+
+	int alphaMode = SCREEN_NON_PRE_MULTIPLIED_ALPHA;
+	err = screen_set_window_property_iv(screen_window, SCREEN_PROPERTY_ALPHA_MODE, &alphaMode);
+	if(err != 0){
+			printf("Failed to set transparency property\n");
 	}
 
 #if 1
@@ -43,7 +55,7 @@ int main(void)
 	}
 #endif
 
-	err = screen_create_window_buffers(screen_window, 1);
+	err = screen_create_window_buffers(screen_window, 2);
 	if(err != 0){
 			printf("Failed to create window buffer\n");
 	}
@@ -54,20 +66,26 @@ int main(void)
 			printf("Failed to get window buffer size\n");
 	}
 
-	screen_buffer_t screen_buf;
+	screen_buffer_t screen_buf[2];
 	err = screen_get_window_property_pv(screen_window, SCREEN_PROPERTY_RENDER_BUFFERS, (void **)&screen_buf);
 	if(err != 0){
 			printf("Failed to get window buffer\n");
 	}
 
 	int *ptr = NULL;
-	err = screen_get_buffer_property_pv(screen_buf, SCREEN_PROPERTY_POINTER, (void **)&ptr);
+	err = screen_get_buffer_property_pv(screen_buf[0], SCREEN_PROPERTY_POINTER, (void **)&ptr);
+	if(err != 0){
+			printf("Failed to get buffer pointer\n");
+	}
+
+	int *ptr1 = NULL;
+	err = screen_get_buffer_property_pv(screen_buf[1], SCREEN_PROPERTY_POINTER, (void **)&ptr1);
 	if(err != 0){
 			printf("Failed to get buffer pointer\n");
 	}
 
 	int stride = 0;
-	err = screen_get_buffer_property_iv(screen_buf, SCREEN_PROPERTY_STRIDE, &stride);
+	err = screen_get_buffer_property_iv(screen_buf[0], SCREEN_PROPERTY_STRIDE, &stride);
 	if(err != 0){
 			printf("Failed to get buffer stride\n");
 	}
@@ -80,9 +98,34 @@ int main(void)
 		"/tmp/test1.png", 100, 100, 1);
 
 	draw_png(ptr, buffer[0], buffer[1], stride,
-		"/tmp/test2.png", 300, 300, 0);
+		"/tmp/test2.png", 300, 300, 1);
 
-	err = screen_post_window(screen_window, screen_buf, 0, NULL, 0);
+        if ( err !=0) {
+		printf("Failed to clear buffer \n");
+	}
+
+        // Setup blit operation
+        int attribs[] = { SCREEN_BLIT_SOURCE_X, 0,
+                          SCREEN_BLIT_SOURCE_Y, 0,
+                          SCREEN_BLIT_SOURCE_WIDTH, buffer[0],
+                          SCREEN_BLIT_SOURCE_HEIGHT, buffer[1],
+                          SCREEN_BLIT_DESTINATION_X, 0,
+                          SCREEN_BLIT_DESTINATION_Y, 0,
+                          SCREEN_BLIT_DESTINATION_WIDTH, buffer[0],
+                          SCREEN_BLIT_DESTINATION_HEIGHT, buffer[1],
+			  SCREEN_BLIT_TRANSPARENCY,SCREEN_TRANSPARENCY_SOURCE_OVER,
+                          SCREEN_BLIT_END };
+
+	// clear dst screen_buf
+	memset(ptr1, 0x00, buffer[0]*buffer[1]*4);
+
+        // Queue blit operation
+        err = screen_blit(screen_context, screen_buf[1],screen_buf[0], attribs);
+	if (err != 0) {
+		printf("Failed to blit buffers \n");
+	}
+
+	err = screen_post_window(screen_window, screen_buf[1], 0, NULL, 0);
 	if(err != 0){
 			printf("Failed to post window\n");
 	}
